8de9f45cfb07e538ac212a40a08389a7d5417693
Giovanni <giovannipor23@gmail.com>
2021-04-12 18:28:20 +0200

    Header (in Vue js)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 var app = new Vue ({
     el: '#root',
+    data: {
+        logo: "img/dark-logo.png",
+        headLinks: [
+            "Home",
+            "Pages",
+            "Courses",
+            "Features",
+            "Blog",
+            "Shop"
+        ],
+        socialIcons: [
+            "fab fa-facebook-f",
+            "fab fa-twitter",
+            "fab fa-instagram",
+            "fab fa-linkedin"
+        ],
+        countdownBtn: "Get Ticket",
+        timer: {
+            icon: "far fa-clock",
+            d: "00",
+            h: "00",
+            m: "00",
+            s: "00"
+        }
+    }
 })
 
