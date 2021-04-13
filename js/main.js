3b34b7fed3d672d19474045ed77528c8078bd25d
Giovanni <giovannipor23@gmail.com>
2021-04-13 12:08:38 +0200

    Footer (in Vue js - 80%)

--- js/main.js
+++ js/main.js
@@ -1,29 +1,159 @@
 var app = new Vue ({
     el: '#root',
     data: {
-        logo: "img/dark-logo.png",
+        logos: [
+            {
+                size: ["xs", "sm", "md", "lg"],
+                title: "MaxCoach",
+                path: "img/",
+                name: "dark-logo",
+                ext: ".png"
+            }
+        ],
+
+        
         headLinks: [
-            "Home",
-            "Pages",
-            "Courses",
-            "Features",
-            "Blog",
-            "Shop"
+            {
+                title: "Home",
+                url: "#"
+            },
+            {
+                title: "Pages",
+                url: "#"
+            },
+            {
+                title: "Courses",
+                url: "#"
+            },
+            {
+                title: "Features",
+                url: "#"
+            },
+            {
+                title: "Blog",
+                url: "#"
+            },
+            {
+                title: "Shop",
+                link: "#"
+            }
         ],
         socialIcons: [
-            "fab fa-facebook-f",
-            "fab fa-twitter",
-            "fab fa-instagram",
-            "fab fa-linkedin"
+            {
+                family: "fab",
+                prefix: "fa-",
+                name: "facebook",
+                suffix: "-f",
+                url: "#"
+            },
+            {
+                family: "fab",
+                prefix: "fa-",
+                name: "twitter",
+                suffix: "",
+                url: "#"
+            },
+            {
+                family: "fab",
+                prefix: "fa-",
+                name: "instagram",
+                suffix: "",
+                url: "#"
+            },
+            {
+                family: "fab",
+                prefix: "fa-",
+                name: "linkedin",
+                suffix: "",
+                url: "#"
+            },
         ],
         countdownBtn: "Get Ticket",
         timer: {
-            icon: "far fa-clock",
+            icon: {
+                family: "far",
+                prefix: "fa-",
+                name: "clock",
+                suffix: "",
+                url: "#",
+            },
             d: "00",
             h: "00",
             m: "00",
             s: "00"
-        }
+        },
+        address: [
+            {
+                header: "Address",
+                url: "#",
+                street: "382 NE 191st",
+                streetNum: 87394,
+                city: "Miami",
+                state: "FL",
+                zip1: 33179,
+                zip2: 3899,
+                intPref: 1,
+                phoneNum: "(305) 547-9909",
+                opTime: "9am",
+                clTime: "5pm",
+                timeZone: "EST",
+                opDay: "Monday",
+                clDay: "Friday",
+                email: "support@maxcoach.com",
+            }
+        ],
+        explore: [
+            {
+                header: "Explore",
+                url: "#",
+                chLinks: [
+                    {
+                        header: "Start Here",
+                        url: "#",
+                    },
+                    {
+                        header: "Success Story",
+                        url: "#",
+                    },
+                    {
+                        header: "Blog",
+                        url: "#",
+                    },
+                    {
+                        header: "Courses",
+                        url: "#",
+                    },
+                    {
+                        header: "Contact us",
+                        url: "#",
+                    },
+                ]
+            }
+        ],
+        information: [
+            {
+                header: "Information",
+                url: "#",
+                chLinks: [
+                    {
+                        header: "Membership",
+                        url: "#",
+                    },
+                    {
+                        header: "Purchase Guide",
+                        url: "#",
+                    },
+                    {
+                        header: "Privacy Policy",
+                        url: "#",
+                    },
+                    {
+                        header: "Terms of services",
+                        url: "#",
+                    },
+                ]
+            }
+        ],
     }
 })
 
